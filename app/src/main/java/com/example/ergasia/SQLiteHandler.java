6983fa0a973c1707e728958cc5c978afb0e3419d
Margoot <lerouzic.margot@gmail.com>
2016-04-11 11:14:38 +0200

    php file to login and register modification of icons and font size

---
+++ app/src/main/java/com/example/ergasia/SQLiteHandler.java
@@ -0,0 +1,131 @@
+package com.example.ergasia;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.HashMap;
+
+/**
+ * This class takes care of storing the user data in SQLite database.
+ * Whenever we needs to get the logged in user information,
+ * we fetch from SQLite instead of making request to server.
+ */
+public class SQLiteHandler extends SQLiteOpenHelper {
+
+    private static final String TAG = SQLiteHandler.class.getSimpleName();
+
+    //All Static variables
+    //Database version
+    private static final int DATABASE_VERSION = 1;
+
+    //Database name
+    private static final String DATABASE_NAME = "Ergasia";
+
+    //Login table name
+    private static final String TABLE_USER = "users";
+
+    //Login table colunms names
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_FIRSTNAME = "firstname";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_UID = "uid";
+    private static final String KEY_CREATED_AT = "created_at";
+
+    public SQLiteHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    //Creating Tables
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_LOGIN_TABLE = "CREATE TABLE " + TABLE_USER + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+                + KEY_FIRSTNAME + " TEXT,"
+                + KEY_EMAIL + " TEXT UNIQUE," + KEY_UID + " TEXT,"
+                + KEY_CREATED_AT + " TEXT" + ")";
+        db.execSQL(CREATE_LOGIN_TABLE);
+
+        Log.d(TAG, "Database tables created");
+    }
+
+    //Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+
+        //Create tables again
+        onCreate(db);
+    }
+
+    /**
+     * Storing user details in database
+     *
+     * @param name
+     * @param firstname
+     * @param email
+     * @param uid
+     * @param created_at
+     */
+    public void addUser(String name, String firstname, String email, String uid, String created_at) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, name); //Name
+        values.put(KEY_FIRSTNAME, firstname); //firstname
+        values.put(KEY_EMAIL, email); //email
+        values.put(KEY_UID, uid); //unique id
+        values.put(KEY_CREATED_AT, created_at); //created_at
+
+        //Inserting Row
+        long id = db.insert(TABLE_USER, null, values);
+        db.close(); //Closing database connection
+
+        Log.d(TAG, "New user inserted into sqlite; " + id);
+    }
+
+    /**
+     * Getting user data from database
+     */
+    public HashMap<String, String> getUserDetails() {
+        HashMap<String, String> user = new HashMap<String, String>();
+        String selectQuery = "SELECT * FROM " + TABLE_USER;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        //Move to first row
+        cursor.moveToFirst();
+        if (cursor.getCount() > 0) {
+            user.put("name", cursor.getString(1));
+            user.put("firstname", cursor.getString(2));
+            user.put("email", cursor.getString(3));
+            user.put("uid", cursor.getString(4));
+            user.put("created_at", cursor.getString(5));
+        }
+
+        cursor.close();
+        db.close();
+        //Return user
+        Log.d(TAG, "Fetching user from Sqlite: " + user.toString());
+        return user;
+    }
+
+    public void deleteUsers() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        //Delete all rows
+        db.delete(TABLE_USER, null , null);
+        db.close();
+
+        Log.d(TAG, "Deleted all user info from Sqlite");
+    }
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/ergasia/SessionManager.java
@@ -0,0 +1,47 @@
+package com.example.ergasia;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.util.Log;
+import android.content.pm.PackageInstaller;
+
+/**
+ * Created by Margot on 11/04/2016.
+ */
+public class SessionManager {
+    //LogCat tag
+    private static String TAG = SessionManager.class.getSimpleName();
+
+    // Shared Preferences
+    SharedPreferences pref;
+
+    Editor editor;
+    Context _context;
+
+    //Shared pref mode
+    int PRIVATE_MODE = 0;
+
+    //Shared preferences file name
+    private static final String PREF_NAME = "ErgasiaLogin";
+    private static final String KEY_IS_LOGGEDIN = "isLoggedIn";
+
+    public SessionManager (Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE); //need multiple shared preferences
+        editor = pref.edit(); //to write to a shared preferences file
+    }
+
+    public void setLogin(boolean isLoggedIn) {
+
+        editor.putBoolean(KEY_IS_LOGGEDIN, isLoggedIn); //Set a boolean value in the preferences editor
+
+        //commit changes
+        editor.commit();
+        Log.d(TAG, "User login session modified");
+    }
+
+    public boolean isLoggedIn () {
+        return pref.getBoolean(KEY_IS_LOGGEDIN, false);
+    }
+}
