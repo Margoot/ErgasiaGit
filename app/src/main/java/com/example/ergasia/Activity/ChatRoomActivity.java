dae0779a16c12d114b3d25bfeb05b84158eb3430
simonthme <thome.simon@gmail.com>
2016-05-14 16:53:08 +0200

    messagerie + notif

--- app/src/main/java/com/example/ergasia/Activity/ChatRoomActivity.java
+++ app/src/main/java/com/example/ergasia/Activity/ChatRoomActivity.java
@@ -7,6 +7,7 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -313,6 +314,16 @@ public class ChatRoomActivity extends AppCompatActivity {
             }
         });
         AppController.getInstance().addToRequestQueue(strReq);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
